7f690e3409c566e16d3962f3bea61961ca1e2699
xd_jiangwenting <2920485521@qq.com>
2024-03-18 20:24:43 +0800

    first comit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Experience.js
+
+程序主入口
+
+# Utils文件夹
+
+工具类
+
+# World文件夹
+
+场景中一下可见的物体
+
+
+
+# 下载
+
+npm install
+
+# 运行
+
+npm  run  dev
+
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="webgl">
+        <canvas ref="canvas" class="canvas"></canvas>
+        <div ref="container"></div>
+    </div>
+</template>
+
+<script setup>
+import Experience from './Experience/Experience.js'
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+
+const canvas = ref(null);
+const container = ref(null);
+
+onMounted(() => {
+    let experience = ref(null);
+    experience = new Experience(
+        canvas.value,
+        container.value
+    );
+
+})
+
+
+</script>
+
+<style scoped>
+.webgl {
+    overflow: clip;
+}
+
+.canvas {
+    overflow: clip;
+    z-index: 0;
+}
+</style>
